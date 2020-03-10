8de898790bfa3d4c1e1a4b6d947ededf1f57535c
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-10 11:12:38 +0100

    Add SURF detector, FLANN matcher computation

--- src/tracking.cpp
+++ src/tracking.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
+#ifdef HAVE_OPENCV_XFEATURES2D
 #include "opencv2/highgui.hpp"
-#include <iostream>
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
 
 using namespace std;
 using namespace cv;
+using namespace cv::xfeatures2d;
 
 int main()
 {
@@ -29,26 +33,57 @@ int main()
   Point point1(24, 44), point2(196, 210);
 
   rectangle(frame, point1, point2, Scalar(0, 0, 255), 1, 8, 0);
+  
   imshow("Bounding Box", frame);
+
+  Mat img_roi = frame(Rect(point1.x, point1.y, point2.x - point1.x, point2.y - point1.y));
+  cvtColor(img_roi, img_roi, CV_RGB2GRAY);
+  
+  imshow("Cropped Bounding Box", img_roi);
 
   waitKey(0);
 
   while (1)
   {
+	cap >> frame;
+	
+    Mat grayscale_frame = frame.clone();
 
-    Mat grayscale = frame.clone();
+    cvtColor(frame, grayscale_frame, CV_RGB2GRAY);
 
-    cvtColor(frame, grayscale, CV_RGB2GRAY);
+    
 
-    cap >> frame;
-
-    imshow("Frame", grayscale);
+    imshow("Frame", grayscale_frame);
 
     // Detect Keypoints using SURF
-    int min_hessian = 300;
+    int min_hessian = 400;
+    std::vector<KeyPoint> keypoints_roi, keypoints_frame;
+    Mat descriptors_roi, descriptors_frame; // NOT A VECTOR !!
+    
+    Ptr<SURF> s_detector = SURF::create(min_hessian);
+    
+    s_detector->detect(img_roi, keypoints_roi);
+    s_detector->compute(img_roi, keypoints_roi, descriptors_roi);
+    
+    s_detector->detect(grayscale_frame, keypoints_frame);
+    s_detector->compute(grayscale_frame, keypoints_frame, descriptors_frame);
+    
 
     // Matching using FLANN
-
+    FlannBasedMatcher matcher;
+    vector< DMatch > matches;
+    matcher.match( descriptors_roi, descriptors_frame, matches);
+    
+    // Draw matches
+    Mat img_match;
+    drawMatches(img_roi, keypoints_roi, grayscale_frame, keypoints_frame, matches, img_match);
+   
+	imshow("Matches", img_match);
+	
+	waitKey(0);
+	
+   
+   
     // If the frame is empty, break immediately
     if (frame.empty())
       break;
@@ -71,3 +106,10 @@ int main()
 
   return 0;
 }
+#else
+
+int main() {
+	cout << "You need the xfeatures2d module to run this program" << endl;
+}
+
+#endif
